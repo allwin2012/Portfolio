a1d19cd5dc8a896112dc37e5dfec3d4c1af71b90
allwin2012 <85178202+allwin2012@users.noreply.github.com>
2023-10-08 21:09:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route, Routes, Route as RouteV6 } from "react-router-dom";
+import "./App.css"; // Make sure you have an App.css file for styling
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidUpdate() {
+    let header = document.querySelector("h1");
+    let text = header.textContent.toLowerCase();
+    let color;
+    switch (text) {
+      case 'about':
+        color = "#16A085";
+        break;
+      case 'projects':
+        color = "#E74C3C";
+        break;
+      case 'contacts':
+        color = "#2980B9";
+        break;
+      default:
+        color = "#F1C40F";
+        break;
+    }
+    let root = document.querySelector(":root");
+    root.style.setProperty("--color-home", color);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav className="Navbar">
+            <h2>A-win</h2>
+            <ul>
+              <li>
+                <Link to="/about">about</Link>
+              </li>
+              <li>
+                <Link to="/projects">projects</Link>
+              </li>
+              <li>
+                <Link to="/contacts">contacts</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <RouteV6 path="/" element={<Home/>} />
+            <RouteV6 path="/about" element={<h1>About</h1>} />
+            <RouteV6 path="/projects" element={<h1>Projects</h1>} />
+            <RouteV6 path="/contacts" element={<h1>Contacts</h1>} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        {/* Other content for the home page */}
+      </div>
+    );
+  }
+}
+
+export default Home;
